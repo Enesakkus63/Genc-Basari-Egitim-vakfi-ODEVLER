5dfc6da1919b43b005b60a438e2dd376775bcd8b
ENES AKKUŞ <148543260+Enesakkus63@users.noreply.github.com>
2025-03-15 20:09:54 +0300

    Pandas odev.py

--- Pandas odev.py
+++ Pandas odev.py
@@ -6,34 +6,34 @@ import seaborn as sns
 
 df = pd.read_csv("C:/Users/enesa/OneDrive/Desktop/Ders/house_prices.csv")
 
-#print(df.head().to_string())
+print(df.head().to_string())
 
-"""""
+
 #Burada gereksiz stunları kaldırıp tamamen boş olan satırları temizleyeceğiz
 df.dropna(axis=1,how='all',inplace=True)
 print(df.head())
 print(df.isnull().sum())
-"""
-"""""
+
+
 # Stunlardaki eksik değerlerin sayısını kontrol etme
  print(df.isnull().sum())
- """
 
-"""""
+
+
 # burada is Price stunlarındaki boş değerleri ortalama ile dolduruyoruz
 df["Price (in rupees)"] = df["Price (in rupees)"].fillna(df["Price (in rupees)"].mean())
 print(df["Price (in rupees)"])
 
-"""
 
-"""""
+
+
 #Şimdi şehirlerdeki lokasyonları gruplayarak ortalama ev fiyalatlarını hesaplayalım
 city_avg_price = df.groupby("location")["Price (in rupees)"].mean().sort_values(ascending=False)
 print(city_avg_price)
-"""
 
 
-"""""
+
+
 # Lokasyon bazında ortalama fiyatları hesaplıyoruz
     df["Price (in rupees)"] = pd.to_numeric(df["Price (in rupees)"], errors="coerce")  # Fiyatları sayısal forma çeviriyoruz
     avg_prices = df.groupby("location")["Price (in rupees)"].mean().sort_values(ascending=False).head(25)  # İlk 25 lokasyonu al
@@ -48,7 +48,7 @@ print(city_avg_price)
     plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.show()
 
-"""
+
 
 
 
